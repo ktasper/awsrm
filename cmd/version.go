073e8d07efbeac8b1afc3491aaabbad24b865f30
Karl Webster <karlwebster97@gmail.com>
2021-11-09 15:19:48 +0000

    Added a version command

---
+++ cmd/version.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Version information",
+	Long:  `Version information`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(version())
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+func version() string {
+	return "0.0.1"
+}
